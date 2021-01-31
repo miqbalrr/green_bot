b7af29f235991808e538326c0169334828445417
miqbalrr <iqb4lcakep@gmail.com>
2021-01-31 16:50:16 +0700

    runn

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::process::Command;
 
 fn main() -> Result<(), ()> {
-    Command::new("git").args(&["add","."]);
-    Command::new("git").args(&["commit","-m", "runn"]);
+    //test
+    Command::new("git").args(&["add","."]).spawn();
+    Command::new("git").args(&["commit","-m", "runn"]).spawn();
     println!("{:?}", Command::new("git").args(&["push","origin","master"]).output());
     Ok(())
 }
