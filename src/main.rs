cbf3cc55fb78e7ac6b5cc295e90d470d5879c76f
miqbalrr <iqb4lcakep@gmail.com>
2021-01-31 22:34:38 +0700

    run at 2021-01-31 15:34:38.434690 UTC

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,30 @@ use std::process::Command;
 use std::fs::File;
 use std::io::prelude::*;
 use chrono::{Utc};
+use std::thread;
+use std::time::Duration;
+use job_scheduler::{JobScheduler, Job};
 
-fn main() -> std::io::Result<()> {
+
+
+fn main() {
+    let mut sched = JobScheduler::new();
+
+    sched.add(Job::new("1/10 * * * * *".parse().unwrap(), || {
+        println!("I get executed every 10 seconds!");
+    }));
+
+    sched.add(Job::new("* * * * * *".parse().unwrap(), || {
+        println!("{:?}",execute_bot());
+    }));
+
+    loop {
+        sched.tick();
+        std::thread::sleep(Duration::from_millis(500));
+    }
+}
+
+fn execute_bot() -> std::io::Result<String> {
     std::fs::remove_file("foo.txt");
     std::fs::remove_file(".git/index.lock");
     let mut file = File::create("foo.txt")?;
@@ -11,6 +33,5 @@ fn main() -> std::io::Result<()> {
     file.write_all(format!("updated at {:?}", now).as_bytes())?;
     Command::new("git").args(&["add","."]).spawn()?;
     Command::new("git").args(&["commit","-m", format!("run at {}", now).as_str()]).spawn()?;
-    print!("{:?}", Command::new("git").args(&["push","origin","master"]).output());
-    Ok(())
+    Ok(format!("{:?}", Command::new("git").args(&["push","origin","master"]).output()))
 }
