19b23a2c69f55273ac7b7250cd232334d56a7f8c
miqbalrr <iqb4lcakep@gmail.com>
2021-01-31 20:41:10 +0700

    runn

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,9 @@ use std::io::prelude::*;
 use chrono::{Utc};
 
 fn main() -> std::io::Result<()> {
-    //test
-    // match file.write_all(format!("update at {}", &Utc::now()).as_bytes()) {
-    //     Ok(..) => {},
-    //     Err(e) => println!("{}", e),
-    // }
+    std::fs::remove_file("foo.txt")?;
     let mut file = File::create("foo.txt")?;
     file.write_all(format!("updated at {:?}", Utc::now()).as_bytes())?;
-
     Command::new("git").args(&["add","."]).spawn();
     Command::new("git").args(&["commit","-m", "runn"]).spawn();
     println!("{:?}", Command::new("git").args(&["push","origin","master"]).output());
