ef7eb0b5760ecf56eb54b24bd62974640aa6bad4
miqbalrr <iqb4lcakep@gmail.com>
2021-01-31 16:47:36 +0700

    init project

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::process::Command;
 fn main() -> Result<(), ()> {
     Command::new("git").args(&["add","."]);
     Command::new("git").args(&["commit","-m", "runn"]);
-    println!("{:?}", Command::new("git").arg("status").output());
+    println!("{:?}", Command::new("git").args(&["push","origin","master"]).output());
     Ok(())
 }
