4fac4e2bfccefa96a31a007e1b664fffe8e3268d
miqbalrr <iqb4lcakep@gmail.com>
2021-01-31 22:36:14 +0700

    run at 2021-01-31 15:36:14.752526 UTC

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,6 @@ use job_scheduler::{JobScheduler, Job};
 fn main() {
     let mut sched = JobScheduler::new();
 
-    sched.add(Job::new("1/10 * * * * *".parse().unwrap(), || {
-        println!("I get executed every 10 seconds!");
-    }));
-
     sched.add(Job::new("* * * * * *".parse().unwrap(), || {
         println!("{:?}",execute_bot());
     }));
