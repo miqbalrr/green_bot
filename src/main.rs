673bd431c3bac1d9949b83f2da626a777df3659f
miqbalrr <iqb4lcakep@gmail.com>
2021-01-31 17:06:49 +0700

    runn

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::process::Command;
+use std::fs::File;
+use std::io::prelude::*;
+use chrono::{Utc};
 
-fn main() -> Result<(), ()> {
+fn main() -> std::io::Result<()> {
     //test
+    let mut file = File::open("file.txt")?;
+    match file.write_all(format!("update at {}", &Utc::now()).as_bytes()) {
+        Ok(..) => {},
+        Err(e) => println!("{}", e),
+    }
     Command::new("git").args(&["add","."]).spawn();
     Command::new("git").args(&["commit","-m", "runn"]).spawn();
     println!("{:?}", Command::new("git").args(&["push","origin","master"]).output());
