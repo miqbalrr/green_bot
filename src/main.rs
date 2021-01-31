9ccb50fc05772ec2292ec63dba1ceb2680e03eff
miqbalrr <iqb4lcakep@gmail.com>
2021-01-31 20:45:47 +0700

    run at 2021-01-31 13:45:47.943869 UTC

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use std::io::prelude::*;
 use chrono::{Utc};
 
 fn main() -> std::io::Result<()> {
-    std::fs::remove_file("foo.txt")?;
+    std::fs::remove_file("foo.txt");
+    std::fs::remove_file(".git/index.lock");
     let mut file = File::create("foo.txt")?;
-    file.write_all(format!("updated at {:?}", Utc::now()).as_bytes())?;
-    Command::new("git").args(&["add","."]).spawn();
-    Command::new("git").args(&["commit","-m", "runn"]).spawn();
+    let now = Utc::now();
+    file.write_all(format!("updated at {:?}", now).as_bytes())?;
+    Command::new("git").args(&["add","."]).spawn()?;
+    Command::new("git").args(&["commit","-m", format!("run at {}", now).as_str()]).spawn()?;
     println!("{:?}", Command::new("git").args(&["push","origin","master"]).output());
     Ok(())
 }
