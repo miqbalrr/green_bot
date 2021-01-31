ce333fef468b6ccf0d6645039acf4a35389be101
miqbalrr <iqb4lcakep@gmail.com>
2021-01-31 17:11:14 +0700

    runn

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ use chrono::{Utc};
 
 fn main() -> std::io::Result<()> {
     //test
-    let mut file = File::open("file.txt")?;
-    match file.write_all(format!("update at {}", &Utc::now()).as_bytes()) {
-        Ok(..) => {},
-        Err(e) => println!("{}", e),
-    }
+    // match file.write_all(format!("update at {}", &Utc::now()).as_bytes()) {
+    //     Ok(..) => {},
+    //     Err(e) => println!("{}", e),
+    // }
+    let mut file = File::create("foo.txt")?;
+    file.write_all(b"Hello, world!")?;
+
     Command::new("git").args(&["add","."]).spawn();
     Command::new("git").args(&["commit","-m", "runn"]).spawn();
     println!("{:?}", Command::new("git").args(&["push","origin","master"]).output());
